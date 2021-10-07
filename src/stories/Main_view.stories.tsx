b3623c2fa9b20bbb3a1c83e29d1341832d8fc81c
dhchun1203 <dhchun1203@gmail.com>
2021-10-07 11:10:58 +0900

    MainView: setting & testing / Dae Hwan

---
+++ src/stories/Main_view.stories.tsx
@@ -0,0 +1,10 @@
+import MainView from "container/layout/Main_view";
+import React from "react";
+
+export default {
+  title: "Main/MainView",
+  decoration: [(Story: any) => <Story />],
+};
+export const MainViewStories = () => {
+  return <MainView />;
+};

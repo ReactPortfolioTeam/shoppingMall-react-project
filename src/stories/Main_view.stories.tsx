83611acdbff54e6ac3b01aa796557ad86a9d26ae
sovla <41351496+sovla@users.noreply.github.com>
2021-10-09 21:05:43 +0900

    Update Main_view.stories.tsx

--- src/stories/Main_view.stories.tsx
+++ src/stories/Main_view.stories.tsx
@@ -6,9 +6,7 @@ export default {
     decoration: [(Story: any) => <Story />],
 };
 export const MainViewStories = () => {
-<<<<<<< HEAD
-  return <MainView />;
-=======
+
     return <MainView />;
->>>>>>> bb16c117fbb48958f364b5913c62c61270fb6717
+
 };

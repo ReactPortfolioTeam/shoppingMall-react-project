52ace8d3c7640acdcb7e8c7a3451f0dfa473d8de
dhchun1203 <dhchun1203@gmail.com>
2021-10-25 16:20:59 +0900

    Carts : product_option_id - value fix

--- src/state/atom/dummy/Carts.ts
+++ src/state/atom/dummy/Carts.ts
@@ -4,27 +4,27 @@ export const Carts = atom({
     key: 'Carts',
     default: [
         {
-            product_option_id: '0001',
+            product_option_id: 1,
             quantity: 1,
         },
         {
-            product_option_id: '0002',
+            product_option_id: 2,
             quantity: 2,
         },
         {
-            product_option_id: '0003',
+            product_option_id: 3,
             quantity: 1,
         },
         {
-            product_option_id: '0004',
+            product_option_id: 4,
             quantity: 3,
         },
         {
-            product_option_id: '0005',
+            product_option_id: 5,
             quantity: 2,
         },
         {
-            product_option_id: '0006',
+            product_option_id: 6,
             quantity: 1,
         },
     ],

cae39a5e5b9ea8a30fca0072cae82a7c301ca2a0
sovla <sovla@naver.com>
2021-10-31 21:40:33 +0900

    Create Cart Atom & Add Item Complete

---
+++ src/state/atom/Cart.ts
@@ -0,0 +1,13 @@
+import { atom } from 'recoil';
+
+export interface CartItem {
+    product_option_id: number;
+    quantity: number;
+}
+
+const Cart = atom<CartItem[]>({
+    key: 'Cart',
+    default: [],
+});
+
+export default Cart;

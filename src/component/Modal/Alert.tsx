7b73f91daeb51f65624dfef199bf814a0f7e4ecc
dhchun1203 <dhchun1203@gmail.com>
2021-12-02 20:32:10 +0900

    AccountInfo: 로그아웃 API 연동 및 Alert 모달창 스타일 수정(z-index)

--- src/component/Modal/Alert.tsx
+++ src/component/Modal/Alert.tsx
@@ -25,6 +25,7 @@ const ModalContainer = styled.div`
     width: 100vw;
     height: 100vh;
     background-color: rgba(0, 0, 0, 0.7);
+    z-index: 9;
     & > div {
         position: fixed;
         display: flex;
--- src/container/Login/AccountInfo/AccountHeader.tsx
+++ src/container/Login/AccountInfo/AccountHeader.tsx
@@ -1,44 +1,76 @@
+import { API } from 'api/API';
 import { FlexBoxDiv } from 'assets/styledComponents/global/globalStyle.style';
+import TextButton from 'component/Button/TextButton';
+import Alert from 'component/Modal/Alert';
 import * as React from 'react';
-import { useRecoilValue } from 'recoil';
+import { useHistory } from 'react-router';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { Modal } from 'state/atom/modal/Modal';
+import User from 'state/atom/User';
 import UserInfo from 'state/atom/UserInfo';
+import styled from 'styled-components';
 
 interface AccoutHeaderProps {}
 
 const AccountHeader: React.FC<AccoutHeaderProps> = () => {
+    const setUser = useSetRecoilState(User);
     const userInfo = useRecoilValue(UserInfo);
+    const setModal = useSetRecoilState(Modal);
+    const history = useHistory();
+    const handleClick = async () => {
+        await API.get('logout').then((res: any) => {
+            console.dir(res, 'res');
+            setUser({});
+            sessionStorage.removeItem('user');
+            setModal({
+                isOpen: true,
+                ModalComponent: Alert,
+                ModalClose: () => {
+                    setModal({ isOpen: false });
+                    history.push('');
+                },
+                ModalContent: res.data.message,
+            });
+        });
+    };
     return (
-        <FlexBoxDiv
-            flexDirection="row"
-            justifyContent="space-between"
-            className="account-header"
-            style={{
-                top: 0,
-                paddingTop: '1.4285714286rem',
-                paddingBottom: '1.4285714286rem',
-                borderBottom: '.0714285714rem solid #dedede',
-                position: 'sticky',
-                zIndex: 9,
-                background: '#fff',
-            }}
-        >
-            <div>
-                <h2>Account</h2>
-                <span>{userInfo.userid}</span>
-            </div>
-            <div>
-                <a
-                    href="#"
-                    style={{
-                        textAlign: 'center',
-                        lineHeight: '1.4285714286rem',
-                    }}
-                >
-                    <span>Sign out</span>
-                </a>
-            </div>
-        </FlexBoxDiv>
+        <AccountHeaderStyle>
+            <FlexBoxDiv
+                flexDirection="row"
+                justifyContent="space-between"
+                className="account-header"
+                // style={{
+                //     top: 0,
+                //     paddingTop: '1.4285714286rem',
+                //     paddingBottom: '1.4285714286rem',
+                //     borderBottom: '.0714285714rem solid #dedede',
+                //     position: 'sticky',
+                //     zIndex: 9,
+                //     background: '#fff',
+                // }}
+            >
+                <div>
+                    <h2>Account</h2>
+                    <span>{userInfo.userid}</span>
+                </div>
+                <div>
+                    <TextButton onClick={handleClick}>
+                        <span>Sign out</span>
+                    </TextButton>
+                </div>
+            </FlexBoxDiv>
+        </AccountHeaderStyle>
     );
 };
+
+const AccountHeaderStyle = styled.div`
+    top: 0;
+    padding-top: 1.4285714286rem;
+    padding-bottom: 1.4285714286rem;
+    border-bottom: 0.0714285714rem solid #dedede;
+    position: sticky;
+    z-index: 1;
+    background: #fff;
+`;
 
 export default AccountHeader;
